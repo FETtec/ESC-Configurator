6f94183a6bfe235f3c3a86bd30f5ae0ac1625605
Carsten Wache <cwache@Carstens-MBP.fritz.box>
2020-06-02 12:52:57 +0200

    update title

--- src/js/launcher_nw.js
+++ src/js/launcher_nw.js
@@ -2,7 +2,7 @@
 
 nw.Window.open('src/main.html', {
     'id': 'fettec-esc-config',
-    'title': 'FETtec ESC configurator v' + chrome.runtime.getManifest().version,
+    'title': 'FETtec Configurator v' + chrome.runtime.getManifest().version,
     "min_width": 900,
     "min_height": 650,
     'position': 'center',
